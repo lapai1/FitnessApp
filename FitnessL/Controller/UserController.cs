b21e065fbd860aa2a40c96566de75773c629a775
Даниил Лапаев <danilfox2005@gmail.com>
2020-05-09 16:31:02 +0300

    Доработана сериализация

--- FitnessL/Controller/UserController.cs
+++ FitnessL/Controller/UserController.cs
@@ -10,25 +10,26 @@ namespace FitnessL.Controller
     public class UserController
     {
         public User User { get;  }
-        public UserController(string name, User user)
+        public UserController(string userName, string genderName,  DateTime birthDate, double weight, double height)
         {
-            User = user ?? throw new ArgumentNullException("Пользователь не может быть равен Null", nameof(user));
+            // make a check
+            var gender = new Gender(genderName);
+            User = new User(userName, gender, birthDate, weight, height);
         }
 
-        public UserController(string name, string gender, DateTime birthdate, double weight, double height)
-        {
-        }
-
+       
+        
+        
         //в будещем создат класс для сохранения
         public void Save()
         {
             var formatter = new BinaryFormatter();
             using(var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
             {
-                formatter.Serialize(fs, User);
+                   formatter.Serialize(fs, User);
             }
         }
-        public User Load()
+        public UserController()
         {
             var formatter = new BinaryFormatter();
 
@@ -37,13 +38,8 @@ namespace FitnessL.Controller
                 // дессиарилизуем только 1 пользователя нужно предусмотреть это в будущем
                 if(formatter.Deserialize(fs) is User user)
                 {
-                    return user;
+                    User = user;
                 }
-                else
-                {
-                    throw new FileLoadException("Не удалось получить данные пользователя из файла", "users.dot");
-                }
-                // вместо эксепштна можно кидать null тем самым сократить код
             }
         }
 
